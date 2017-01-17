3fd8738cc5adcb9616186557058d029f6ca1ef09
Jason An <anzhe0629@gmail.com>
2017-01-17 12:30:37 -0800

    5 reg

--- function.py
+++ function.py
@@ -55,20 +55,24 @@ tt_ims, tt_labels = tt_ims[0:tt_num], tt_labels[0:tt_num]
 # print(z)
 # z = (numpy.array(z) - min(z)) / (max(z) - min(z)) * 255
 # print(z)
-# print(mndata.display(z))
-    
-plot4(2,3)
-plot4(2,8)
+# # print(mndata.display(z))
+#
+# plot4(2,3)
+# plot4(2,8)
 
 
 # 5. Regularization
 
 Lambda = 0.01
+import numpy, math
+a = 2
+b = 3
 
+# def plot5-J_L1(a, b):
 # def J_L2(t, y, w):
 #     return - sum(t * numpy.log(y) + (1 - numpy.array(t)) * numpy.log(1 - numpy.array(y))) / len(t) + Lambda * dot(w, w)
-# def J_L1(t, y, w):
-#     return - sum(t * numpy.log(y) + (1 - numpy.array(t)) * numpy.log(1 - numpy.array(y))) / len(t) + Lambda * sum(absolute(w))
+def J_L1(t, y, w):
+    return - sum(t * numpy.log(y) + (1 - numpy.array(t)) * numpy.log(1 - numpy.array(y))) / len(t) + Lambda * sum(numpy.absolute(w))
 
 def Y(w, X):
 	return [1 / (1 + math.exp(-numpy.array(w).dot(x))) for x in X]
@@ -87,29 +91,32 @@ hdX, hdT = enX[n:], enT[n:]
 w = numpy.zeros(len(trX[0]))
 
 w_hist = numpy.zeros([4, len(w)])
-hdE_hist = numpy.zeros(4)
+hdJ_hist = numpy.zeros(4)
 
 print(eta)
 print(Lambda)
 print('t, trPC, hdPC, enPC')
+
+fout = open('eta_J1{}{}.csv'.format(a, b), 'w')
 fout.write('eta_L2 = {}\n'.format(eta))
 fout.write('t, rPC, hdPC, enPC\n')
 
 t = 0
 
-while(t < 100 and not((hdE_hist[-4] < hdE_hist[-3]) and (hdE_hist[-3] < hdE_hist[-2]) and (hdE_hist[-2] < hdE_hist[-1]))):
-	y = Y(w, trX)
-	w = w + [eta * sum((trT[n] - y[n]) * trX[n][j] for n in range(len(trX))) for j in range(len(w))] - 2 * Lambda * w
-	trY, hdY, enY = Y(w, trX), Y(w, hdX), Y(w, enX)
-	trPC, hdPC, enPC = PC(trT, trY), PC(hdT, hdY), PC(enT, enY)
+while(t < 100 and not((hdJ_hist[-4] < hdJ_hist[-3]) and (hdJ_hist[-3] < hdJ_hist[-2]) and (hdJ_hist[-2] < hdJ_hist[-1]))):
+    y = Y(w, trX)
+    w = w + [eta * sum((trT[n] - y[n]) * trX[n][j] for n in range(len(trX))) for j in range(len(w))] - 2 * Lambda * w
+
+    trY, hdY, enY = Y(w, trX), Y(w, hdX), Y(w, enX)
+    hdJ = J_L1(hdT, hdY, w)
+    trPC, hdPC, enPC = PC(trT, trY), PC(hdT, hdY), PC(enT, enY)
 
     w_hist = numpy.append(w_hist,w)
-	hdE_hist = numpy.append(hdE_hist, [hdE], axis = 0)
-
+    hdJ_hist = numpy.append(hdJ_hist, [hdJ], axis = 0)
     print(t, trPC, hdPC, enPC)
     fout.write('{}, {}, {}, {}\n'.format(t, trPC, hdPC, enPC))
 
-    t = t + 1
+    t += 1
 fout.close()
 
 
